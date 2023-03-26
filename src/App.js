0ce3c83de32fd9cd18898f2dd21c411fa1a7115f
Sergio Gontardo <sergiogontardo@gmail.com>
2023-03-26 19:12:26 -0400

    primera versión de calculadora

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState} from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const numeros = () => {
+    const numeros = [];
+      for (let i = 1; i < 10; i++) {
+        numeros.push(
+          <button onClick={() => operacion(i.toString())} key={i}>{i}</button>
+        )
+    }
+      return numeros;
+  }
+
+  const [calculo, setCalculo] = useState("");
+  const [resultado, setResultado] = useState(""); 
+  
+  const operadores = ["+", "-", "*", "/", "."];
+
+  const operacion = valor => {
+		if (
+			(operadores.includes(valor) && calculo === '') || 
+      (operadores.includes(valor) && operadores.includes(calculo.slice(-1)))
+		) {
+			return;
+		}
+		setCalculo(calculo + valor);
+
+		if (!operadores.includes(valor)) {
+			setResultado(eval(calculo + valor).toString());
+		}
+	}
+
+  const calcular = () => {
+    setCalculo(eval(calculo).toString());
+  }
+
+  const borrar = () => {
+    if(calculo === "") {
+        return;
+    } else {
+        const valor = calculo.slice(0, -1);
+        setCalculo(valor); 
+    }
+  }
+  
+  const clear = () => {
+    if (calculo === "") {
+        return;
+    } else {
+      const valor = calculo.slice(0, -100);
+      setCalculo(valor);
+    }
+  }
+
+return (
+  <div className='app'>
+    <div className='calculadora'>
+      <div className='resultado'>
+        { calculo || "0" }
+      </div>
+      <div className='operadores'>
+        
+        <button onClick={ () => operacion('+') }>+</button>
+        <button onClick={ () => operacion('-') }>-</button>
+        <button onClick={ () => operacion('*') }>x</button>
+        <button onClick={ () => operacion('/') }>÷</button>
+
+        <button onClick={ borrar }>DEL</button>
+        <button onClick={ clear }>AC</button>
+
+      </div>
+      <div className='numeros'>
+        { numeros() } 
+        <button onClick={ () => operacion('0')}>0</button>
+        <button onClick={ () => operacion('.') }>.</button>
+        <button onClick={calcular}>=</button>
+      </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
